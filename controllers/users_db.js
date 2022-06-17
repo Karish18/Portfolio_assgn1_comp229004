31b9a53a20179bbe243f980d23f7a6319d0e7057
Karish18 <105962754+Karish18@users.noreply.github.com>
2022-06-17 00:22:13 -0400

    asgn

--- controllers/users_db.js
+++ controllers/users_db.js
@@ -1,18 +1,116 @@
 //provide reference of the user_db.js in the models folder
-let userDb = require('../models/users_db');
+let userModel = require('../models/users_db');
 
 module.exports.userList = function(req,res,next){
 
-    userDb.find((err,userList) =>{
+    userModel.find((err,userList) =>{
          if (err)
          {
              return console.error(err);
          }
          else
          {
-             console.log(userList);
-            // res.render('Users/List',{title : 'Users List',UserList: userList})
+             //console.log(userList);
+            res.render('users_db/List',{title : 'Users List',UserList: userList})
 
          }
+    });
+}
+
+module.exports.displayEditPage = (req, res, next) => {
+    let id = req.params.id;
+
+    userModel.findById(id, (err, userToEdit) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('users_db/add_edit', {
+                title: 'Edit User', 
+                user: userToEdit
+            })
+        }
+    });
+}
+
+
+module.exports.processEditPage = (req, res, next) => {
+    let id = req.params.id
+
+    let updatedUser = userModel({
+        _id: req.body.id,
+        email_id: req.body.email_id,
+        user_name: req.body.username,
+        contact: req.body.contact,
+        tags: req.body.tags.split(",").map(word => word.trim())
+    });
+
+
+userModel.updateOne({_id: id}, updatedUser, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            res.redirect('/users_db/list');
+        }
+    });
+}
+
+
+module.exports.displayAddPage = (req, res, next) => {
+    let newUser = userModel();
+
+    res.render('users_db/add_edit', {
+        title: 'Add a new User',
+        user: newUser
+    })          
+}
+
+module.exports.processAddPage = (req, res, next) => {
+
+    let newUser = userModel({
+        _id: req.body.id,
+        email_id: req.body.email_id,
+        user_name: req.body.username,
+        contact: req.body.contact,
+        tags: req.body.tags.split(",").map(word => word.trim())
+    });
+
+    userModel.create(newUser, (err, user) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            console.log(user);
+            res.redirect('/users_db/list');
+        }
+    });
+
+}
+
+
+module.exports.performDelete = (req, res, next) => {
+    let id = req.params.id;
+
+    userModel.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            res.redirect('/users_db/list');
+        }
     });
 }
--- routes/users_db.js
+++ routes/users_db.js
@@ -5,4 +5,17 @@ let user_dbController = require('../controllers/users_db');
 router.get('/list',user_dbController.userList);
 
 
+// edit routers
+router.get('/edit/:id', user_dbController.displayEditPage);
+router.post('/edit/:id', user_dbController.processEditPage);
+
+// Display the Add page 
+router.get('/add', user_dbController.displayAddPage);
+
+//Process the Add page  
+router.post('/add', user_dbController.processAddPage);
+
+// process the delete page 
+router.get('/delete/:id', user_dbController.performDelete);
+
 module.exports = router;
